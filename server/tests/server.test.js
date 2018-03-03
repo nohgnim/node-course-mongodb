a10425a49ee81b4c23bb2241b1d0f6ef42ca52b6
minghon <mhon@clearstreaminc.com>
2018-03-02 17:06:33 -0700

    Add tests for /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,9 +3,18 @@ const request = require('supertest')
 const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
 
+const todos = [{
+    text: 'First todo'
+}, {
+    text: 'Second todo'
+}]
+
 //Deletes all the records out of the database each time the test runs
+//Insert 2 test todos
 beforeEach((done) => {
-    Todo.remove({}).then(() => done ())
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos)
+    }).then(() => done())
 })
 
 test('Should create a new todo', (done)=>{
@@ -13,7 +22,7 @@ test('Should create a new todo', (done)=>{
 
     request(app)
         .post('/todos')
-        .send({text})
+        .send({text}) //Send in request parameters as object
         .expect(200)
         .expect((res) => {
             expect(res.body.text).toBe(text)
@@ -23,7 +32,7 @@ test('Should create a new todo', (done)=>{
                 return done(err)
             }
 
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1)
                 expect(todos[0].text).toBe(text)
                 done()
@@ -43,8 +52,19 @@ test('Should not create a todo with invalid body data', (done)=>{
             }
 
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(0)
+                expect(todos.length).toBe(2)
                 done()
             }).catch((e) => done(e))
         })
+})
+
+test('Should get all todos', (done)=>{
+    //Make request passing in app
+    request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todos.length).toBe(2)
+        })
+        .end(done)
 })
