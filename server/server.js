9af3da241e93f0358bba95e8e1b3233ca43559e9
minghon <mhon@clearstreaminc.com>
2018-03-05 21:38:49 -0700

    Add get todos by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
 let {mongoose} = require('./db/mongoose')
 let {Todo} = require('./models/todo')
@@ -29,6 +30,25 @@ app.get('/todos', (req,res) => {
         res.status(400).send(err)
     }
 })
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id
+    if(!ObjectID.isValid(id)){
+        res.status(404).send()
+        return console.log('Id is not valid')
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            res.status(404).send()
+            return console.log('Id not found.')
+        }
+        res.status(200).send({todo})
+    }).catch((err) => {
+        res.status(400).send()
+    })
+}), (err) => {
+    res.status(400).send(err)
+}
 
 app.listen(3000, () => {
     console.log('Started on port 3000.')
