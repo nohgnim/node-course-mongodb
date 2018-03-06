e4e4cc121c866bea172149c0c8dbff7318b791b9
minghon <mhon@clearstreaminc.com>
2018-03-06 15:53:45 -0700

    Test the delete todos by id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -20,78 +20,120 @@ beforeEach((done) => {
     }).then(() => done())
 })
 
-test('Should create a new todo', (done)=>{
-    let text = 'Poop'
-
-    request(app)
-        .post('/todos')
-        .send({text}) //Send in request parameters as object
-        .expect(200)
-        .expect((res) => {
-            expect(res.body.text).toBe(text)
-        })
-        .end((err, res) => {
-            if(err){
-                return done(err)
-            }
-
-            Todo.find({text}).then((todos) => {
-                expect(todos.length).toBe(1)
-                expect(todos[0].text).toBe(text)
-                done()
-            }).catch((e) => done(e))
-        })
+describe('Todo get', ()=>{
+    test('Should create a new todo', (done)=>{
+        let text = 'Poop'
+    
+        request(app)
+            .post('/todos')
+            .send({text}) //Send in request parameters as object
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text).toBe(text)
+            })
+            .end((err, res) => {
+                if(err){
+                    return done(err)
+                }
+    
+                Todo.find({text}).then((todos) => {
+                    expect(todos.length).toBe(1)
+                    expect(todos[0].text).toBe(text)
+                    done()
+                }).catch((e) => done(e))
+            })
+    })
+    
+    test('Should not create a todo with invalid body data', (done)=>{
+        //Make request passing in app
+        request(app)
+            .post('/todos')
+            .send() //pass in invalid empty request
+            .expect(400)
+            .end((err, res) => {
+                if(err){
+                    return done(err)
+                }
+    
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(2)
+                    done()
+                }).catch((e) => done(e))
+            })
+    })
+    
+    test('Should get all todos', (done)=>{
+        //Make request passing in app
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2)
+            })
+            .end(done)
+    })
+    
+    test('Should return todo document', (done)=>{
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text)
+            })
+            .end(done)
+    })
+    
+    test('Should return 404 if todo not found', (done)=>{
+        request(app)
+            .get(`/todos/${new ObjectId().toHexString()}`)
+            .expect(404)
+            .end(done)
+    })
+    
+    test('Should return 404 if not an object id', (done)=>{
+        request(app)
+            .get(`/todos/1234`)
+            .expect(404)
+            .end(done)
+    })
 })
 
-test('Should not create a todo with invalid body data', (done)=>{
-    //Make request passing in app
-    request(app)
-        .post('/todos')
-        .send() //pass in invalid empty request
-        .expect(400)
-        .end((err, res) => {
-            if(err){
-                return done(err)
-            }
+describe('Remove functions', ()=>{
+    //Test delete route
+    test('Should return 404 if not an object id', (done)=>{
+        request(app)
+            .delete(`/todos/1234`)
+            .expect(404)
+            .end(done)
+    })
 
-            Todo.find().then((todos) => {
-                expect(todos.length).toBe(2)
-                done()
-            }).catch((e) => done(e))
-        })
-})
+    test('Should return 404 if todo not found', (done)=>{
+        request(app)
+            .delete(`/todos/${new ObjectId().toHexString()}`)
+            .expect(404)
+            .end(done)
+    })
 
-test('Should get all todos', (done)=>{
-    //Make request passing in app
-    request(app)
-        .get('/todos')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body.todos.length).toBe(2)
-        })
-        .end(done)
-})
-
-test('Should return todo document', (done)=>{
-    request(app)
-        .get(`/todos/${todos[0]._id.toHexString()}`)
-        .expect(200)
-        .expect((res) => {
-            expect(res.body.todo.text).toBe(todos[0].text)
-        })
-        .end(done)
-})
-
-test('Should return 404 if todo not found', (done)=>{
-    request(app)
-        .get(`/todos/${new ObjectId().toHexString()}`)
-        .expect(404)
-        .end(done)
-})
-
-test('Should return 404 if not an object id', (done)=>{
-    request(app)
-        .get(`/todos/1234`)
-        .expect(404)
-        .end(done)
+    test('Should delete one todo', (done)=>{
+        //Make request passing in app
+        let hexId = todos[0]._id.toHexString()
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId)
+            })
+            .end((err, res)=>{
+                if(err){
+                    return done(err)
+                }
+                //Query db to try to find the item, it should return null
+                Todo.findById(hexId).then((todos)=>{
+                    expect(todos).toBeNull()
+                    done()
+                }).catch((err)=>{
+                    done(err)
+                })
+            })
+    })
 })
